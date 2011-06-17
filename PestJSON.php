b92e4ead635521085117d884d8fe07885f10c164
Egbert <egbert.teeselink@siouxbinc.com>
2011-06-17 19:52:54 +0800

    Bugfixes

--- PestJSON.php
+++ PestJSON.php
@@ -9,32 +9,33 @@ require_once 'Pest.php';
  * 
  * In other words, while Pest's get/post/put/delete calls return raw strings,
  * PestJSON return (associative) arrays.
+ * 
+ * In case of >= 400 status codes, an exception is thrown with $e->getMessage() 
+ * containing the error message that the server produced. User code will have to 
+ * json_decode() that manually, if applicable, because the PHP Exception base
+ * class does not accept arrays for the exception message.
  *
  * See http://github.com/educoder/pest for details.
  *
  * This code is licensed for use, modification, and distribution
  * under the terms of the MIT License (see http://en.wikipedia.org/wiki/MIT_License)
  */
-public class PestJSON extends Pest
+class PestJSON extends Pest
 {
   public function post($url, $data) {
-    parent::post($url, json_encode($data));
+    return parent::post($url, json_encode($data));
   }
   
   public function put($url, $data) {
-    parent::put($url, json_encode($data));
+    return parent::put($url, json_encode($data));
   }
 
   protected function prepRequest($opts, $url) {
     $opts[CURLOPT_HTTPHEADER][] = 'Content-Type: application/json';
+    return parent::prepRequest($opts, $url);
   }
 
   public function processBody($body) {
     return json_decode($body, true);
   }
-  
-  public function processError($body) {
-    return json_decode($body, true);
-  }
-
 }
