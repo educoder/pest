31a3688952194c9078623c053be9033b9e78181a
Egbert <egbert.teeselink@siouxbinc.com>
2011-06-17 20:30:23 +0800

    Minor doccomment fixes

--- PestJSON.php
+++ PestJSON.php
@@ -3,6 +3,8 @@
 require_once 'Pest.php';
 
 /**
+ * Small Pest addition by Egbert Teeselink (http://www.github.com/eteeselink)
+ *
  * Pest is a REST client for PHP.
  * PestJSON adds JSON-specific functionality to Pest, automatically converting
  * JSON data resturned from REST services into PHP arrays and vice versa.
@@ -13,7 +15,8 @@ require_once 'Pest.php';
  * In case of >= 400 status codes, an exception is thrown with $e->getMessage() 
  * containing the error message that the server produced. User code will have to 
  * json_decode() that manually, if applicable, because the PHP Exception base
- * class does not accept arrays for the exception message.
+ * class does not accept arrays for the exception message and some JSON/REST servers
+ * do not produce nice JSON 
  *
  * See http://github.com/educoder/pest for details.
  *
