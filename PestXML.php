18c3126d667806b7429083ee0c4db866c46e50a0
Matt Zandstra <matthew@avenirit.com>
2012-04-18 16:31:28 -0500

    fix for empty xml docs like http://pastebin.com/gdT2V37y

--- PestXML.php
+++ PestXML.php
@@ -29,11 +29,15 @@ class PestXML extends Pest {
     
     if (!$xml) {
       $err = "Couldn't parse XML response because:\n";
-      foreach(libxml_get_errors() as $xml_err)
-        $err .= "\n    - " . $xml_err->message;
-      $err .= "\nThe response was:\n";
-      $err .= $body;
-      throw new PestXML_Exception($err);
+      $xml_errors = libxml_get_errors();
+      if(!empty($xml_errors))
+      {
+        foreach(libxml_get_errors() as $xml_err)
+          $err .= "\n    - " . $xml_err->message;
+        $err .= "\nThe response was:\n";
+        $err .= $body;
+        throw new PestXML_Exception($err);
+      }
     }
     
     return $xml;
