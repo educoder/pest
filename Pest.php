e1c7ada1b20ce7005f279cfd9a5f8453243f998b
Omar Gonzalez <omar@almerblank.com>
2012-04-21 22:59:08 -0700

    Updated the addHeader() to use a try/catch block. The is_array() line was crashing if CURLOPT_HTTPHEADER didn't exist. Using the try/catch block fixed the error.

--- Pest.php
+++ Pest.php
@@ -44,9 +44,9 @@ class Pest {
   // Lets you add a header to the header list
   // Example: pest->addHeader("Accept", "application/xml");
   public function addHeader($headerName, $value) {
-      if (is_array($this->curl_opts[CURLOPT_HTTPHEADER])) {
+      try {
           array_push($this->curl_opts[CURLOPT_HTTPHEADER], $headerName.":".$value);
-      } else {
+      } catch (Exception $e) {
           $this->curl_opts[CURLOPT_HTTPHEADER] = array($headerName.":".$value);
       }
   }
