8f71f77c7763d7ac1024b20dd680a4da8d32d82f
arielstt <ariel@trustedtranslations.net>
2014-11-04 14:33:24 -0300

    Skip SSL Host Verification

--- Pest.php
+++ Pest.php
@@ -15,6 +15,7 @@ class Pest
     public $curl_opts = array(
         CURLOPT_RETURNTRANSFER => true, // return result instead of echoing
         CURLOPT_SSL_VERIFYPEER => false, // stop cURL from verifying the peer's certificate
+        CURLOPT_SSL_VERIFYHOST => false, // stop cURL from verifying the peer's HOST
         CURLOPT_FOLLOWLOCATION => false, // follow redirects, Location: headers
         CURLOPT_MAXREDIRS => 10, // but dont redirect more than 10 times
         CURLOPT_HTTPHEADER => array()
